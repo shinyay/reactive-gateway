24c8ea3ecf1d208f96c9da570f9a92d2e293bb4e
shinyay <shinya.com@gmail.com>
2021-11-01 16:13:19 +0900

    Add RouteLocator for Gateway

--- src/main/kotlin/io/spring/shinyay/gateway/GatewayReactiveApplication.kt
+++ src/main/kotlin/io/spring/shinyay/gateway/GatewayReactiveApplication.kt
@@ -2,10 +2,21 @@ package io.spring.shinyay.gateway
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class GatewayReactiveApplication
+class GatewayReactiveApplication {
+
+    @Bean
+    fun gateway(rlb: RouteLocatorBuilder) = rlb.routes()
+        .route { r ->
+            r.path("/proxy").and().host("*.spring.io")
+                .filters { f -> f.setPath("/customers") }
+                .uri("http://localhost:8080")
+        }.build()
+}
 
 fun main(args: Array<String>) {
-	runApplication<GatewayReactiveApplication>(*args)
+    runApplication<GatewayReactiveApplication>(*args)
 }

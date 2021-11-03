989934f0d5206cca8328c5b33dd24e3d071a8110
shinyay <shinya.com@gmail.com>
2021-11-03 22:02:05 +0900

    Add a controller for Customer

--- src/main/kotlin/io/spring/shinyay/gateway/controller/CustomerController.kt
+++ src/main/kotlin/io/spring/shinyay/gateway/controller/CustomerController.kt
@@ -1,4 +1,19 @@
 package io.spring.shinyay.gateway.controller
 
+import io.spring.shinyay.gateway.client.OrderClient
+import io.spring.shinyay.gateway.entity.Customer
+import io.spring.shinyay.gateway.entity.CustomerOrders
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Flux
+
+
+@RestController
 class CustomerController {
+    private val client: OrderClient? = null
+
+    @GetMapping("/customer")
+    fun get(): Flux<Customer?>? {
+        return client?.getCustomers()
+    }
 }
